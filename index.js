62df2576dbb385205815ad7cc76ea9dd28911916
Whien <sal95610@gmail.com>
2016-11-30 14:54:23 +0800

    call walk flow improve

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var execSync = require('child_process').execSync
 var packageJSON = require(process.cwd() + '/package.json')
 
 var package
+var packagelen
 var mode
 var i = 0;
 var limit = 5;
@@ -19,13 +20,14 @@ var packageJSONDependenciesKey = Object.keys(packageJSON.dependencies || {})
 var packageJSONDevDependenciesKey = Object.keys(packageJSON.devDependencies || {})
 
 const optionDefinitions = [
-  { name: 'package', alias: 'p', defaultOption: true },
+  { name: 'package', alias: 'p', defaultOption: true, multiple: true },
   { name: 'mode', alias: 'm' },
   { name: 'help' },
   { name: 'version', alias: 'V' }
 ]
 
 const options = commandLineArgs(optionDefinitions)
+
 package = options.package
 mode = options.mode || 'save'
 
@@ -37,127 +39,144 @@ program
   .parse(process.argv)
 
 keypress(process.stdin);
-console.log(`Link to http://registry.npmjs.org/${package}`)
-var req = http.get(`http://registry.npmjs.org/${package}`, function(res) {
-  let data = ''
-  res.setEncoding('utf8')
-  res.on('data', (chunk) => data += chunk)
-  res.on('end', () => {
-    try {
-      let parseData = JSON.parse(data)
-      var versionKeys = Object.keys(parseData.versions)
-      pagination = Math.ceil(versionKeys.length / limit)
-      callDown(versionKeys, 'new')
-      process.stdin.on('keypress', function (ch, key) {
-        switch(key.name) {
-          case 'space':
-            if (page + 1 > pagination) {
-              return
-            }
-            i = page * (limit) - 1
-            page++
-            callDown(versionKeys)
-          break;
-          case 'down':
-          case 'j':
-            callDown(versionKeys)
-            break
-          case 'up':
-          case 'k':
-            callUp(versionKeys)
-            break
-          case 'l':
-            if (mode === 'dev') {
-              execSync(`yarn add --dev ${package}@latest`, (error, stdout, stderr) => {
-                console.log(stdout)
-              })
-            } else {
-              execSync(`yarn add ${package}@latest`, (error, stdout, stderr) => {
-                console.log(stdout)
-              })
-            }
-            exit()
-          break
-          case 'n':
-            if (mode === 'dev') {
-              execSync(`yarn add --dev ${package}@next`, (error, stdout, stderr) => {
-                console.log(stdout)
-              })
-            } else {
-              execSync(`yarn add ${package}@next`, (error, stdout, stderr) => {
-                console.log(stdout)
-              })
-            }
-            exit()
-          break
-          case 't':
-            console.log(versionKeys)
-          break
-          case 'r':
-            if (packageJSONDependenciesKey.indexOf(package) !== -1) {
-              clear()
-              console.log(`remove ${package} v${packageJSON.dependencies[package]}..`)
-              exec(`yarn remove ${package}`, (error, stdout, stderr) => {
-                console.log(stdout)
-              })
+
+function walk(package) {
+  console.log(`Link to http://registry.npmjs.org/${package}`)
+  var req = http.get(`http://registry.npmjs.org/${package}`, function(res) {
+    let data = ''
+    res.setEncoding('utf8')
+    res.on('data', (chunk) => data += chunk)
+    res.on('end', () => {
+      try {
+        let parseData = JSON.parse(data)
+        var versionKeys = Object.keys(parseData.versions)
+        pagination = Math.ceil(versionKeys.length / limit)
+        callDown(versionKeys, 'new')
+        process.stdin.on('keypress', function (ch, key) {
+          switch(key.name) {
+            case 'space':
+              if (page + 1 > pagination) {
+                return
+              }
+              i = page * (limit) - 1
+              page++
+              callDown(versionKeys)
+            break;
+            case 'down':
+            case 'j':
+              callDown(versionKeys)
+              break
+            case 'up':
+            case 'k':
+              callUp(versionKeys)
+              break
+            case 'l':
+              if (mode === 'dev') {
+                execSync(`yarn add --dev ${package}@latest`, (error, stdout, stderr) => {
+                  console.log(stdout)
+                })
+              } else {
+                execSync(`yarn add ${package}@latest`, (error, stdout, stderr) => {
+                  console.log(stdout)
+                })
+              }
               exit()
-            } else {
-              console.log(`you didn't install ${package}`.red)
-            }
-          break
-          case 'v':
-            execSync(`yarn --version`, (error, stdout, stderr) => {
-              console.log(`yarn version ${stdout}`)
-            })
-          break
-          case 'return':
-            clear()
-            console.log(`install ${package} ${versionKeys[i]}..`)
-            if (mode === 'dev') {
-              exec(`yarn add --dev ${package}@${versionKeys[i]}`, (error, stdout, stderr) => {
-                console.log(stdout)
+            break
+            case 'n':
+              if (mode === 'dev') {
+                execSync(`yarn add --dev ${package}@next`, (error, stdout, stderr) => {
+                  console.log(stdout)
+                })
+              } else {
+                execSync(`yarn add ${package}@next`, (error, stdout, stderr) => {
+                  console.log(stdout)
+                })
+              }
+              exit()
+            break
+            case 't':
+              console.log(versionKeys)
+            break
+            case 'r':
+              if (packageJSONDependenciesKey.indexOf(package) !== -1) {
+                clear()
+                console.log(`remove ${package} v${packageJSON.dependencies[package]}..`)
+                exec(`yarn remove ${package}`, (error, stdout, stderr) => {
+                  console.log(stdout)
+                })
+                exit()
+              } else {
+                console.log(`you didn't install ${package}`.red)
+              }
+            break
+            case 'v':
+              execSync(`yarn --version`, (error, stdout, stderr) => {
+                console.log(`yarn version ${stdout}`)
               })
-            } else {
-              exec(`yarn add ${package}@${versionKeys[i]}`, (error, stdout, stderr) => {
-                console.log(stdout)
-              })
-            }
-            exit()
-          break
-        }
-        if (key && key.ctrl && key.name == 'd') {
-          if (mode !== 'dev') {
-            mode = 'dev'
-          } else {
-            mode = 'save'
+            break
+            case 'return':
+              clear()
+              console.log(`install ${package} ${versionKeys[i]}..`)
+              if (mode === 'dev') {
+                exec(`yarn add --dev ${package}@${versionKeys[i]}`, (error, stdout, stderr) => {
+                  console.log(stdout)
+                })
+              } else {
+                exec(`yarn add ${package}@${versionKeys[i]}`, (error, stdout, stderr) => {
+                  console.log(stdout)
+                })
+              }
+              exit()
+            break
           }
-          callDown(versionKeys, 'new')
-        }
-        if (key && key.ctrl && key.name == 'g') {
-          clear()
-          console.log(`Download https://registry.npmjs.org/${package}/-/${package}-${versionKeys[i]}.tgz ...`)
-          execSync(`wget https://registry.npmjs.org/${package}/-/${package}-${versionKeys[i]}.tgz`, (error, stdout, stderr) => {
-            console.log(stdout)
-          })
-          exit()
-        }
-        if (key && key.ctrl && key.name == 'c') {
-          exit()
-        }
-      })
-      process.stdin.setRawMode(true);
-      process.stdin.resume();
-    } catch (e) {
-      console.log(`[select npm] >> Can't not find ${package}`)
-    }
+          if (key && key.ctrl && key.name == 'd') {
+            if (mode !== 'dev') {
+              mode = 'dev'
+            } else {
+              mode = 'save'
+            }
+            callDown(versionKeys, 'new')
+          }
+          if (key && key.ctrl && key.name == 'g') {
+            clear()
+            console.log(`Download https://registry.npmjs.org/${package}/-/${package}-${versionKeys[i]}.tgz ...`)
+            execSync(`wget https://registry.npmjs.org/${package}/-/${package}-${versionKeys[i]}.tgz`, (error, stdout, stderr) => {
+              console.log(stdout)
+            })
+            exit()
+          }
+          if (key && key.ctrl && key.name == 'c') {
+            exit()
+          }
+        })
+        process.stdin.setRawMode(true);
+        process.stdin.resume();
+      } catch (e) {
+        console.log(`[select npm] >> Can't not find ${package}`)
+      }
+    })
   })
-})
+  req.on('error', function(e) {
+    console.log(e)
+  })
 
-req.on('error', function(e) {
-  console.log(e)
-})
+  req.end()
+}
 
-req.end()
+// main
+packagelen = (typeof package === 'object' && package.length) > 1 ? package.length : 1
+if (package && packagelen === 1) {
+  walk(package[0])
+} else if (packagelen > 1) {
+  clear()
+  console.log(`Didn't support multiple package now.`)
+} else {
+  exec(`nyi --help`, (error, stdout, stderr) => {
+    console.log(stdout)
+  })
+}
+
+// util
 
 function clear() {
   process.stdout.write('\033[2J')
